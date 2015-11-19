b067f73b3fa9573fa03824a48515d9324ca50ff6
Jeffrey Johnson <jjphatt@gmail.com>
2015-11-19 15:38:01 -0800

    Moved Tetgen mesh construction into polyglot.
    Also improved testing CMake stuff.

---
+++ cmake/Modules/add_polyglot_test.cmake
@@ -0,0 +1,35 @@
+# This function adds a (serial) unit test executable to be built using cmockery.
+function(add_polyglot_test exe)
+  add_executable(${exe} ${ARGN})
+  target_link_libraries(${exe} cmockery ${POLYGLOT_LIBRARIES})
+  set_target_properties(${exe} PROPERTIES COMPILE_FLAGS "-DCMAKE_CURRENT_SOURCE_DIR=\\\"${CMAKE_CURRENT_SOURCE_DIR}\\\"")
+  add_test(${exe} ${exe})
+  set_tests_properties(${exe} PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+endfunction()
+
+# This function adds a parallel unit test executable to be built using gtest.
+# The procs argument is a list of numbers of processes to be run.
+# 1 test run will be generated for each processor number value.
+function(add_mpi_polyglot_test exe)
+  foreach (arg ${ARGN})
+    if (arg MATCHES ".c")
+      list(APPEND sources ${arg})
+    else()
+      list(APPEND procs ${arg})
+    endif()
+  endforeach()
+  add_executable(${exe} ${sources})
+  target_link_libraries(${exe} cmockery ${POLYGLOT_LIBRARIES})
+  set_target_properties(${exe} PROPERTIES COMPILE_FLAGS "-DCMAKE_CURRENT_SOURCE_DIR=\\\"${CMAKE_CURRENT_SOURCE_DIR}\\\"")
+  if (HAVE_MPI EQUAL 1)
+    foreach (proc ${procs})
+      add_test(${exe}_${proc}_proc ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${proc} ${MPIEXEC_PREFLAGS} ${CMAKE_CURRENT_BINARY_DIR}/${exe} ${MPIEXEC_POSTFLAGS})
+      set_tests_properties(${exe}_${proc}_proc PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+    endforeach()
+  else()
+    # We only add a single-process test case when MPI is not present.
+    add_test(${exe}_1_proc ${CMAKE_CURRENT_BINARY_DIR}/${exe})
+    set_tests_properties(${exe}_1_proc PROPERTIES WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+  endif()
+endfunction()
+
---
+++ polyglot/import_tetgen_mesh.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2015, Jeffrey N. Johnson
+// All rights reserved.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef POLYGLOT_IMPORT_TETGEN_MESH_H
+#define POLYGLOT_IMPORT_TETGEN_MESH_H
+
+#include "core/mesh.h"
+#include "core/point.h"
+
+// This function imports a mesh using .node, .ele, .face, and .neigh files 
+// created by TetGen. A global mesh is constructed on process 0 and is then 
+// partitioned onto the given communicator using an unweighted partitioning 
+// algorithm with a maximum load imbalance ratio of 0.05.
+mesh_t* import_tetgen_mesh(MPI_Comm comm, 
+                           const char* node_file,
+                           const char* ele_file,
+                           const char* face_file,
+                           const char* neigh_file);
+
+#endif
+
