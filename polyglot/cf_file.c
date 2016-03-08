6de680e354a73cbd121fe0a72b524f5d257ee7db
Jeffrey Johnson <jjphatt@gmail.com>
2016-03-07 22:08:59 -0800

    Implemented a good deal of cf_file.

--- polyglot/cf_file.c
+++ polyglot/cf_file.c
@@ -5,31 +5,366 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#include "netcdf.h"
 #include "polyglot/cf_file.h"
+
+#if POLYMEC_HAVE_DOUBLE_PRECISION
+#define NC_REAL NC_DOUBLE
+#else
+#define NC_REAL NC_FLOAT
+#endif
 
 struct cf_file_t 
 {
-  bool valid;
-  char error[1024];
+  int file_id;
+  int cf_major_version, cf_minor_version, cf_patch_version;
+  bool writing;
 };
+
+// Helpers.
+static void get_first_attribute(int file_id, 
+                                int var_id, 
+                                const char* attr,
+                                char* value)
+{
+  size_t attr_len;
+  int err = nc_inq_attlen(file_id, var_id, attr, &attr_len);
+  if (err != NC_NOERR)
+  {
+    polymec_error("cf_file: Error retrieving length of attribute %s: %s", 
+                  attr, nc_strerror(err));
+  }
+  if (attr_len > 1)
+  {
+    char attrs[attr_len][NC_MAX_NAME+1];
+    int err = nc_get_att_string(file_id, var_id, attr, (char**)&attrs[0]);
+    if (err != NC_NOERR)
+    {
+      polymec_error("cf_file: Error retrieving attribute %s: %s", 
+                    attr, nc_strerror(err));
+    }
+  }
+  else
+  {
+    int err = nc_get_att_string(file_id, var_id, attr, &value);
+    if (err != NC_NOERR)
+    {
+      polymec_error("cf_file: Error retrieving attribute %s: %s", 
+                    attr, nc_strerror(err));
+    }
+  }
+}
+
+static void get_first_global_attribute(int file_id,
+                                       const char* attr, 
+                                       char* value)
+{
+  get_first_attribute(file_id, NC_GLOBAL, attr, value);
+}
+
+static void put_attribute(int file_id, 
+                          int var_id, 
+                          const char* attr,
+                          const char* value)
+{
+  int err = nc_put_att_string(file_id, var_id, attr, 1, &value);
+  if (err != NC_NOERR)
+  {
+    polymec_error("cf_file: Error setting attribute %s: %s", 
+                  attr, nc_strerror(err));
+  }
+}
 
 cf_file_t* cf_file_new(const char* filename)
 {
+  int file_id;
+  int err = nc_open(filename, NC_WRITE, &file_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_new: Couldn't open file %s: %s", filename, nc_strerror(err));
+
+  // Create our representation.
   cf_file_t* cf = polymec_malloc(sizeof(cf_file_t));
+  cf->file_id = file_id;
+  cf->cf_major_version = 1;
+  cf->cf_minor_version = 6;
+  cf->cf_patch_version = 0;
+  cf->writing = true;
+
+  // Write in our conventions.
+  char conventions[NC_MAX_NAME+1];
+  snprintf(conventions, NC_MAX_NAME, "CF-%d.%d.%d", cf->cf_major_version, 
+           cf->cf_minor_version, cf->cf_patch_version);
+  err = nc_put_att_string(cf->file_id, NC_GLOBAL, "Conventions", 1, (const char**)&conventions);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_new: Couldn't write Conventions attribute: %s", nc_strerror(err));
+
   return cf;
 }
 
-bool cf_file_is_valid(cf_file_t* file)
+cf_file_t* cf_file_open(const char* filename)
 {
-  return file->valid;
-}
+  int file_id;
+  int err = nc_open(filename, NC_NOWRITE, &file_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_open: Couldn't open file %s: %s", filename, nc_strerror(err));
 
-char* cf_file_error(cf_file_t* file)
-{
-  return &file->error[0];
+  char conventions[NC_MAX_NAME+1];
+  get_first_global_attribute(file_id, "Conventions", conventions);
+  if (((conventions[0] != 'c') && (conventions[0] != 'C')) || 
+      ((conventions[1] != 'f') && (conventions[1] != 'F')) || 
+      (conventions[2] != '-') || (strlen(conventions) < 4))
+  {
+    nc_close(file_id);
+    polymec_error("cf_file_open: File %s is not a CF-compliant NetCDF file.", filename);
+  }
+
+  // Create our representation.
+  cf_file_t* cf = polymec_malloc(sizeof(cf_file_t));
+  cf->file_id = file_id;
+  cf->cf_major_version = cf->cf_minor_version = cf->cf_patch_version = 0;
+  cf->writing = false;
+
+  // Parse the CF conventions version numbers from the string.
+  int num;
+  char** versions = string_split(&conventions[3], ".", &num);
+  ASSERT(num > 0);
+  cf->cf_major_version = atoi(versions[0]);
+  if (num > 1)
+    cf->cf_minor_version = atoi(versions[1]);
+  if (num > 2)
+    cf->cf_patch_version = atoi(versions[2]);
+  for (int i = 0; i < num; ++i)
+    string_free(versions[i]);
+  polymec_free(versions);
+
+  return cf;
 }
 
 void cf_file_close(cf_file_t* file)
+{
+  nc_close(file->file_id);
+}
+
+void cf_file_get_version(cf_file_t* file, 
+                         int* major_version,
+                         int* minor_version,
+                         int* patch_version)
+{
+  *major_version = file->cf_major_version;
+  *minor_version = file->cf_minor_version;
+  *patch_version = file->cf_patch_version;
+}
+
+void cf_file_get_provenance(cf_file_t* file, 
+                            char* title,
+                            char* institution,
+                            char* source,
+                            char* history,
+                            char* references,
+                            char* comment)
+{
+  cf_file_get_global_attribute(file, "title", title);
+  cf_file_get_global_attribute(file, "institution", institution);
+  cf_file_get_global_attribute(file, "source", source);
+  cf_file_get_global_attribute(file, "history", history);
+  cf_file_get_global_attribute(file, "references", references);
+  cf_file_get_global_attribute(file, "comment", comment);
+}
+
+void cf_file_set_provenance(cf_file_t* file, 
+                            const char* title,
+                            const char* institution,
+                            const char* source,
+                            const char* history,
+                            const char* references,
+                            const char* comment)
+{
+  ASSERT(file->writing);
+  cf_file_set_global_attribute(file, "title", title);
+  cf_file_set_global_attribute(file, "institution", institution);
+  cf_file_set_global_attribute(file, "source", source);
+  cf_file_set_global_attribute(file, "history", history);
+  cf_file_set_global_attribute(file, "references", references);
+  cf_file_set_global_attribute(file, "comment", comment);
+}
+
+void cf_file_set_global_attribute(cf_file_t* file, 
+                                  const char* global_attribute_name,
+                                  const char* value)
+{
+  ASSERT(file->writing);
+  put_attribute(file->file_id, NC_GLOBAL, global_attribute_name, value);
+}
+
+void cf_file_get_global_attribute(cf_file_t* file, 
+                                  const char* global_attribute_name,
+                                  char* value)
+{
+  get_first_global_attribute(file->file_id, global_attribute_name, value);
+}
+
+void cf_file_define_dimension(cf_file_t* file,
+                              const char* dimension_name,
+                              int value)
+{
+  ASSERT(file->writing);
+  if (value == -1)
+    value = NC_UNLIMITED;
+  int id;
+  int err = nc_def_dim(file->file_id, dimension_name, value, &id);
+  if (err != NC_NOERR)
+  {
+    polymec_error("cf_file_define_dimension: Could not define dimension %s: %s",
+                  dimension_name, nc_strerror(err));
+  }
+}
+
+int cf_file_dimension(cf_file_t* file, const char* dimension_name)
+{
+  int id;
+  int err = nc_inq_dimid(file->file_id, dimension_name, &id);
+  if (err != NC_NOERR)
+  {
+    polymec_error("cf_file_dimension: Could not retrieve ID for dimension %s: %s",
+                  dimension_name, nc_strerror(err));
+  }
+  size_t dim;
+  err = nc_inq_dimlen(file->file_id, id, &dim);
+  if (err != NC_NOERR)
+  {
+    polymec_error("cf_file_dimension: Could not retrieve dimension %s: %s",
+                  dimension_name, nc_strerror(err));
+  }
+  return (int)dim;
+}
+
+void cf_file_define_latlon_grid(cf_file_t* file,
+                                real_t* latitude_points,
+                                int num_latitude_points,
+                                const char* latitude_units,
+                                real_t* longitude_points,
+                                int num_longitude_points,
+                                const char* longitude_units,
+                                real_t* vertical_points,
+                                int num_vertical_points,
+                                const char* vertical_units,
+                                const char* vertical_orientation)
+{
+  ASSERT(num_latitude_points > 0);
+  ASSERT(num_longitude_points > 0);
+  ASSERT(num_vertical_points > 0);
+
+  // Latitude dimension, data, metadata.
+  int dim_id, var_id;
+  int err = nc_def_dim(file->file_id, "lat", num_latitude_points, &dim_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not define lat dimension: %s", nc_strerror(err));
+  err = nc_def_var(file->file_id, "lat", NC_REAL, 1, &dim_id, &var_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not define lat variable: %s", nc_strerror(err));
+  err = nc_put_var(file->file_id, var_id, latitude_points);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not set lat data: %s", nc_strerror(err));
+  put_attribute(file->file_id, var_id, "long_name", "latitude");
+  put_attribute(file->file_id, var_id, "standard_name", "latitude");
+  put_attribute(file->file_id, var_id, "units", latitude_units);
+
+  // Longitude metadata.
+  err = nc_def_dim(file->file_id, "lon", num_longitude_points, &dim_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not define lon dimension: %s", nc_strerror(err));
+  err = nc_def_var(file->file_id, "lon", NC_REAL, 1, &dim_id, &var_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not define lon variable: %s", nc_strerror(err));
+  err = nc_put_var(file->file_id, var_id, longitude_points);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not set lon data: %s", nc_strerror(err));
+  put_attribute(file->file_id, var_id, "long_name", "longitude");
+  put_attribute(file->file_id, var_id, "standard_name", "longitude");
+  put_attribute(file->file_id, var_id, "units", longitude_units);
+
+  // Vertical metadata.
+  err = nc_def_dim(file->file_id, "lev", num_vertical_points, &dim_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not define lev dimension: %s", nc_strerror(err));
+  err = nc_def_var(file->file_id, "lev", NC_REAL, 1, &dim_id, &var_id);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not define lev variable: %s", nc_strerror(err));
+  err = nc_put_var(file->file_id, var_id, vertical_points);
+  if (err != NC_NOERR)
+    polymec_error("cf_file_define_latlon_grid: Could not set lev data: %s", nc_strerror(err));
+  put_attribute(file->file_id, var_id, "long_name", "longitude");
+  put_attribute(file->file_id, var_id, "standard_name", "longitude");
+  put_attribute(file->file_id, var_id, "units", vertical_units);
+  put_attribute(file->file_id, var_id, "positive", vertical_orientation);
+  put_attribute(file->file_id, var_id, "axis", "Z");
+}
+
+bool cf_file_has_latlon_grid(cf_file_t* file)
+{
+}
+
+void cf_file_get_latlon_grid_metadata(cf_file_t* file,
+                                      int* num_latitude_points,
+                                      char* latitude_units,
+                                      int* num_longitude_points,
+                                      char* longitude_units,
+                                      int* num_vertical_points,
+                                      char* vertical_units,
+                                      char* vertical_orientation)
+{
+}
+
+void cf_file_get_latlon_points(cf_file_t* file,
+                               real_t* latitude_points,
+                               real_t* longitude_points,
+                               real_t* vertical_points)
+{
+}
+
+void cf_file_define_time(cf_file_t* file,
+                         const char* time_units,
+                         const char* calendar)
+{
+}
+
+void cf_file_get_time_metadata(cf_file_t* file,
+                               char* time_units,
+                               char* calendar)
+{
+}
+
+int cf_file_set_time(cf_file_t* file, real_t t)
+{
+}
+
+void cf_file_define_latlon_var(cf_file_t* file, 
+                               const char* var_name,
+                               const char* short_name,
+                               const char* long_name,
+                               const char* units)
+{
+}
+
+void cf_file_get_latlon_var_metadata(cf_file_t* file, 
+                                     const char* var_name,
+                                     char* short_name,
+                                     char* long_name,
+                                     char* units)
+{
+}
+
+void cf_file_write_latlon_var(cf_file_t* file, 
+                              const char* var_name,
+                              int time_index, 
+                              real_t* var_data)
+{
+}
+
+void cf_file_read_latlon_var(cf_file_t* file, 
+                             const char* var_name,
+                             int time_index, 
+                             real_t* var_data)
 {
 }
 
--- polyglot/cf_file.h
+++ polyglot/cf_file.h
@@ -14,23 +14,186 @@
 // files adhering to the Climate/Forecast conventions, the details of which 
 // are available at http://cfconventions.org/.
 
+// Maximum length of an identifier name in CF files. Shouldn't exceed name 
+// limits in the underlying NetCDF library.
+#define POLYGLOT_CF_MAX_NAME 256
+
 // This type provides the interface for CF files.
 typedef struct cf_file_t cf_file_t;
 
-// Opens a new CF file for reading simulation data, returning the CF 
+// Opens a new CF file for writing simulation data, returning the CF 
 // file object. 
 cf_file_t* cf_file_new(const char* filename);
 
-// Returns true if the CF file was parsed successfully upon construction 
-// and contains valid data, false if not.
-bool cf_file_is_valid(cf_file_t* file);
+// Opens an existing CF file for reading simulation data, returning the CF 
+// file object, or NULL if the file is not found or can't be opened, OR if 
+// the file is a NetCDF file that doesn't follow the CF conventions.
+cf_file_t* cf_file_open(const char* filename);
 
-// Returns a pointer to an internal string containing any error message(s) 
-// related to the parsing of the CF file. If the CF file was parsed 
-// and the object is valid, this returns the empty string.
-char* cf_file_error(cf_file_t* file);
-
-// Closes and destroys the given CF file.
+// Closes and destroys the given CF file handle. If the CF file was opened 
+// for writing, this flushes all buffers to disk.
 void cf_file_close(cf_file_t* file);
+
+// Retrieves the major, minor, and patch version of the CF conventions 
+// that the contents of this file follow. 
+void cf_file_get_version(cf_file_t* file, 
+                         int* major_version,
+                         int* minor_version,
+                         int* patch_version);
+
+// Retrieves strings identifying the provenance and contents of this CF file. 
+// These identifiers are described at cfconventions.org:
+//   title - a succinct description of what is in the dataset.
+//   institution - specifies where the original data was produced.
+//   source - the method of production of the original data. If it was 
+//            model-generated, source should name the model and its version, 
+//            as specifically as could be useful. If it is observational, 
+//            source should characterize it (e.g., "surface observation" or 
+//            "radiosonde").
+//   history - provides an audit trail for modifications to the original data. 
+//             Well-behaved generic netCDF filters will automatically append 
+//             their name and the parameters with which they were invoked to 
+//             the global history attribute of an input netCDF file. We 
+//             recommend that each line begin with a timestamp indicating the 
+//             date and time of day that the program was executed.
+//   references - published or web-based references that describe the data or 
+//                methods used to produce it.
+//   comment - miscellaneous information about the data or methods used to 
+//             produce it.
+// Each of these strings must be able to hold POLYGLOT_CF_MAX_NAME+1 
+// characters. If a given value is not found, its string will be set to the 
+// empty string.
+void cf_file_get_provenance(cf_file_t* file, 
+                            char* title,
+                            char* institution,
+                            char* source,
+                            char* history,
+                            char* references,
+                            char* comment);
+
+// Sets the provenance information (described above) for the given file, which 
+// must have been opened for writing.
+void cf_file_set_provenance(cf_file_t* file, 
+                            const char* title,
+                            const char* institution,
+                            const char* source,
+                            const char* history,
+                            const char* references,
+                            const char* comment);
+
+// Sets the value of the given global attribute.
+void cf_file_set_global_attribute(cf_file_t* file, 
+                                  const char* global_attribute_name,
+                                  const char* value);
+
+// Fetches the value of the given global attribute into value, which must be 
+// able to hold POLYGLOT_CF_MAX_NAME+1 characters.
+void cf_file_get_global_attribute(cf_file_t* file, 
+                                  const char* global_attribute_name,
+                                  char* value);
+
+// Sets up a generic dimension with the given numeric value, or -1 if 
+// the dimension is considered to be "unlimited" (like a time series).
+void cf_file_define_dimension(cf_file_t* file,
+                              const char* dimension_name,
+                              int value);
+
+// Retrieves the value of the given dimension. -1 corresponds to an 
+// "unlimited" dimension.
+int cf_file_dimension(cf_file_t* file, const char* dimension_name);
+
+// Sets up the data for a latitude/longitude grid with the given number 
+// of latitudinal/longitudinal/vertical grid points and units of measure.
+// Possible units of measure are:
+//   latitude: degree_north, degree_N, degrees_N, degreeN, degreesN
+//   longitude: degree_east, degree_E, degrees_E, degreeE, degreesE
+//   vertical: bar, millibar, decibar, atmosphere (atm), pascal (Pa), hPa,
+//             meter (metre, m), kilometer (km), 
+//             level, layer, sigma_level
+// Additionally, an orientiation (vertical_orientation, "up" or "down") must 
+// be specified.
+void cf_file_define_latlon_grid(cf_file_t* file,
+                                real_t* latitude_points,
+                                int num_latitude_points,
+                                const char* latitude_units,
+                                real_t* longitude_points,
+                                int num_longitude_points,
+                                const char* longitude_units,
+                                real_t* vertical_points,
+                                int num_vertical_points,
+                                const char* vertical_units,
+                                const char* vertical_orientation);
+
+// Returns true if the CF file contains a lat-lon grid, false if not.
+bool cf_file_has_latlon_grid(cf_file_t* file);
+
+// Fetches information about the latlon grid in the given file (if present).
+// Strings should all be able to store POLYGLOT_CF_MAX_NAME+1 characters.
+void cf_file_get_latlon_grid_metadata(cf_file_t* file,
+                                      int* num_latitude_points,
+                                      char* latitude_units,
+                                      int* num_longitude_points,
+                                      char* longitude_units,
+                                      int* num_vertical_points,
+                                      char* vertical_units,
+                                      char* vertical_orientation);
+
+// Fetches latitude/longitude/vertical grid points to the given arrays, which 
+// must be large enough to fit them.
+void cf_file_get_latlon_points(cf_file_t* file,
+                               real_t* latitude_points,
+                               real_t* longitude_points,
+                               real_t* vertical_points);
+
+// Sets up the time variable within the CF file, specifying units and 
+// a calendar to use. Available units are:
+//   day (d), hour (hr, h), minute (min), second (sec, s)
+// Plural forms may be used as well, and "since xyz" where xyz is a timestamp
+// may be used to specify times relative to an epoch. Available calendars are:
+//   gregorian (standard), proleptic_gregorian, noleap (365_day), 
+//   all_leap (366_day), 360_day, julian, none
+void cf_file_define_time(cf_file_t* file,
+                         const char* time_units,
+                         const char* calendar);
+
+// Retrieves time information (units and calendar) to strings large enough to 
+// hold NC_NAME_MAX+1 characters.
+void cf_file_get_time_metadata(cf_file_t* file,
+                               char* time_units,
+                               char* calendar);
+
+// Sets the time with which calls following this one will be associated. Returns 
+// an integer index identifying the given time.
+int cf_file_set_time(cf_file_t* file, real_t t);
+
+// Defines a variable that is defined on the points of a lat-lon grid, 
+// setting up metadata like short and long names and units.
+void cf_file_define_latlon_var(cf_file_t* file, 
+                               const char* var_name,
+                               const char* short_name,
+                               const char* long_name,
+                               const char* units);
+
+// Fetches metadata for the given lat-lon variable. All strings must 
+// be large enough to hold POLYGLOT_CF_MAX_NAME+1 characters.
+void cf_file_get_latlon_var_metadata(cf_file_t* file, 
+                                     const char* var_name,
+                                     char* short_name,
+                                     char* long_name,
+                                     char* units);
+
+// Writes a variable that is defined on the points of a lat-lon grid, 
+// specifying a time index that associates this entry with a given time.
+void cf_file_write_latlon_var(cf_file_t* file, 
+                              const char* var_name,
+                              int time_index, 
+                              real_t* var_data);
+
+// Reads a variable that is defined on the points of a lat-lon grid, 
+// specifying an index for the time at which the data will be read.
+void cf_file_read_latlon_var(cf_file_t* file, 
+                             const char* var_name,
+                             int time_index, 
+                             real_t* var_data);
 
 #endif
